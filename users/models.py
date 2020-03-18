816e0f4bc74de581e1f8f211342a147ac8000a3e
Gavin Hsu <gavinhsu0621@gmail.com>
2020-03-18 12:18:03 +0800

    memberdb

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    Name = models.CharField(max_length=10)
+    ID = models.CharField(max_length=10, primary_key=True)
+    Gender = models.CharField(max_length=5)
+    Email = models.CharField(max_length=50)
+    Phone = models.CharField(max_length=10)
+    Address = models.CharField(max_length=100)
+    Account = models.CharField(max_length=20)
+    Password = models.CharField(max_length=20)
+    BDay = models.DateField(null=False)
+    Photo = models.URLField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        result = str(self.ID) + ' ' + str(self.Name)
+        return result
