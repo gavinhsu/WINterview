2b6084b3b747d67e599a458ad86685ce5643db66
Gavin Hsu <gavinhsu0621@gmail.com>
2020-03-26 22:37:22 +0800

    homepage&signUp

--- MockInterview/settings.py
+++ MockInterview/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'questions',
     'users',
     'gcp',
+    'templates',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static'),
+]
--- MockInterview/urls.py
+++ MockInterview/urls.py
@@ -11,11 +11,15 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.jurls'))
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+import users.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', users.views.homepage),
+    path('users/', include("users.url")),
 ]
