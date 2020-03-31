162e827680fac2e8869e7576755c0a277cd45df8
yangtsunjun <eddie880312@yahoo.com.tw>
2020-03-31 15:57:22 +0800

    rename

--- gcp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GcpConfig(AppConfig):
-    name = 'gcp'
--- gcp/url.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-import gcp.views
-
-path('speech_to_text/', gcp.HelloViews.View.as_view()),
