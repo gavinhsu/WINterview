1d4204dd9eeb5625699cd736b6c09545a24406ca
yangtsunjun <eddie880312@yahoo.com.tw>
2020-06-07 17:42:43 +0800

    speech_to_text display output on template

--- GTTS/url.py
+++ GTTS/url.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
 #from django.contrib.auth.views import SpeechView
-import GTTS.views
+import gtts.tests.test_tts.views
 
 app_name = 'GTTS'
 
 urlpatterns = [
   path('speech_to_text/', GTTS.views.Speech),
+  #path(button_click, views.),
+  #path(r'^speech_to_text/$', Speech.as_view())
  ]
--- GTTS/views.py
+++ GTTS/views.py
@@ -6,44 +6,50 @@ from gtts import gTTS
 from pygame import mixer
 import tempfile
 
+
 # Create your views here.
 class Speech(TemplateView):
     template_name = 'speech_to_text.html'
     
     def get(self, request):
-        s = self.speak()
+        s = self.convert_text()
         return render(request, self.template_name, {
             "s" : s,
         })
 
-    def speak(self, sentence, lang, loops):
-        self.sentence = sentence
-        self.lang = lang
-        self.loops = 1
 
-        with tempfile.NamedTemporaryFile(delete = False) as fp:
-            tts = gTTS(text=sentence, lang=lang)
-            tts.save('{}.mp3'.format(fp.name))
-            # tts.save('test.mp3')
-            mixer.init()
-            mixer.music.load('{}.mp3'.format(fp.name))
-            # mixer.music.load('test.mp3')
-            mixer.music.play(loops)
+    #def speak(self, sentence, lang, loops=1):
 
+        #with tempfile.NamedTemporaryFile(delete = False) as fp:
+            # tts = gTTS(text=sentence, lang=lang)
+            # tts.save('{}.mp3'.format(fp.name))
+            # # tts.save('test.mp3')
+            # mixer.init()
+            # mixer.music.load('{}.mp3'.format(fp.name))
+            # # mixer.music.load('test.mp3')
+            # mixer.music.play(loops)
+
+    # def button_click(self, request):
+    #     if(request.Get.get('speech_button')):
+    #         buttonClicked = "hello"
+    #         print("button clicked")                                                     
+    #     return render(request, 'speech_to_text.html', {'buttonClicked':buttonClicked})
+
+
+    def convert_text(self):
         # obtain audio from the microphone
         r = sr.Recognizer()
         with sr.Microphone() as source:
             print("Please wait. Calibrating microphone...")
-        # listen for 0.5 seconds and create the ambient noise energy level
-        r.adjust_for_ambient_noise(source, duration=0.5)
-        print("Say something!")
-        audio = r.listen(source)
+            # listen for 0.5 seconds and create the ambient noise energy level
+            r.adjust_for_ambient_noise(source, duration=5)
+            print("Say something!")
+            audio = r.listen(source)
 
         # recognize speech using Google Speech Recognition
         try:
-            print("Your speech::>>>")
-        # print(r.recognize_google(audio, language="zh-TW"))
-            s = r.recognize_google(audio, language="zh-TW")
+            print("Your speech is ===>")
+            s = r.recognize_google(audio, language="en-us")
             print(s)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
@@ -51,5 +57,6 @@ class Speech(TemplateView):
             print("No response from Google Speech Recognition service: {0}".format(e))
         return s
 
-    if __name__ == '__speak__':
-        speak()
+    if __name__ == '__convert_text__':
+        convert_text()
+        #speak(sentence=s, lang='en-us', loops=1)
