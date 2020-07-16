e45c512e8bc8297c0036e858c2e97b57a37cbe01
JavaIsShit <henry880122555@gmail.com>
2020-07-16 17:08:31 +0800

    video uploading test

--- questions/admin.py
+++ questions/admin.py
@@ -4,11 +4,11 @@ from .models import Investment_Banking
 from .models import Answer
 from .models import Result
 from .models import Record
-
+from .models import Video
 # Register your models here.
 admin.site.register(Software_Engineer)
 admin.site.register(Investment_Banking)
 admin.site.register(Answer)
 admin.site.register(Result)
 admin.site.register(Record)
-
+admin.site.register(Video)
